4b6d6e523816b6eed9a76ac23f1cd68679cb78d7
Takey Tajwar <tajwar.earth@pm.me>
2026-01-21 17:12:24 +0600

    write back to the socket

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use tokio::io::{AsyncReadExt};
+use tokio::io::{AsyncReadExt,AsyncWriteExt};
 use tokio::net::TcpListener;
 
 #[tokio::main]
@@ -11,6 +11,7 @@ async fn main() -> tokio::io::Result<()> {
 	tokio::spawn(async move {
 	    let mut buf = [0; 1024];
 	    let bytes_read = match socket.read(&mut buf).await {
+		Ok(0) => return,
 		Ok(n) => n,
 		Err(err) => {
 		    eprintln!("Error from reading socket: {:?}", err);
@@ -18,6 +19,20 @@ async fn main() -> tokio::io::Result<()> {
 		},
 	    };
 	    println!("{:?}", &buf[..bytes_read]);
+
+	    let msg = format!(
+		"HTTP/1.1 200 OK\r\n\
+		 Content-Type: text/plain\r\n\
+		 Content-Length: {}\r\n\
+		 \r\n\
+		 OK",
+		"OK".len()
+	    );
+
+
+	    if let Err(err) = socket.write_all(msg.as_bytes()).await {
+		eprintln!("Error from writing to socket: {:?}", err);
+	    }
 	});
     }
 }
