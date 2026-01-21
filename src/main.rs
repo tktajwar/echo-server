b66ef2366b82dc3f3155857c80033af8c53a106d
Takey Tajwar <tajwar.earth@pm.me>
2026-01-21 20:54:34 +0600

    echo back the received data until EOF signal
    Return the received message. Loop until EOF.

--- src/main.rs
+++ src/main.rs
@@ -9,29 +9,25 @@ async fn main() -> tokio::io::Result<()> {
 	let (mut socket, _) = listener.accept().await?;
 
 	tokio::spawn(async move {
+	    println!("-- New Connection --");
 	    let mut buf = [0; 1024];
-	    let bytes_read = match socket.read(&mut buf).await {
-		Ok(0) => return,
-		Ok(n) => n,
-		Err(err) => {
-		    eprintln!("Error from reading socket: {:?}", err);
-		    0
-		},
-	    };
-	    println!("{:?}", &buf[..bytes_read]);
+	    loop {
+		let bytes_read = match socket.read(&mut buf).await {
+		    Ok(0) => {
+			println!("-- Connection Closed --");
+			return
+		    },
+		    Ok(n) => n,
+		    Err(err) => {
+			eprintln!("Error from reading socket: {:?}", err);
+			0
+		    },
+		};
+		println!("{:?}", &buf[..bytes_read]);
 
-	    let msg = format!(
-		"HTTP/1.1 200 OK\r\n\
-		 Content-Type: text/plain\r\n\
-		 Content-Length: {}\r\n\
-		 \r\n\
-		 OK",
-		"OK".len()
-	    );
-
-
-	    if let Err(err) = socket.write_all(msg.as_bytes()).await {
-		eprintln!("Error from writing to socket: {:?}", err);
+		if let Err(err) = socket.write_all(&buf[..bytes_read]).await {
+		    eprintln!("Error from writing to socket: {:?}", err);
+		}
 	    }
 	});
     }
